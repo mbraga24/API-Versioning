e618382d28ce5a280b2ef13bd7e75b202836b1bc
github username <rdg.brg@gmail.com>
2020-08-23 18:20:53 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+product_numbers = [*1..8]
+
+product_numbers.each do |num|
+  Product.create(
+    name: "Product name_#{num}",
+    brand: "Product brand_#{num}",
+    price: "$#{num}00.00",
+    description: "Product description_#{num}"
+  )
+end
+
+puts "Optional visual feedback when it finishes seeding."
+puts "=================================================="
+puts "                    SEEDED"
+puts "=================================================="
